b594c73c666c7c20bd855871473925282017135c
manuellopper <96139661+manuellopper@users.noreply.github.com>
2023-09-24 23:11:31 +0200

    Actualizado con la ayuda de ChatGPT

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hola Mundo")
+import csv
+
+with open('data.csv') as File:
+    reader = csv.reader(File)
+    for row in reader:
+        print(row)
+
+import csv
+
+with open('data.csv') as File:
+    reader = csv.DictReader(File)
+    for row in reader:
+        print(row['nombre'], row['edad'])
